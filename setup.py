d0828632ae8948146a371e7aa07f78299cd2c6ce
KunFang <fangkun930601@me.com>
2025-02-23 10:00:05 -0500

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='Nuc3DMap',
+    version='1.0',
+    py_modules=['Nuc3DMap'],
+    install_requires=[
+        'Click','scipy','numpy','matplotlib','pandas','seaborn','statsmodels','cooler','numba','psutil','pyranges','tqdm','deeptools','bwa','pairtools','pybedtools'
+    ],
+    entry_points='''
+    [console_scripts]
+    Nuc3DMap=Nuc3DMap:cli
+    '''
+)
