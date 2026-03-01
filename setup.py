34ab46feaaf29516a8f664f0024ac26d782b490c
KunFang <fangkun930601@me.com>
2026-03-01 17:17:56 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='Nuc3DMap',
-    version='1.0',
+    version='1.1',
     py_modules=['Nuc3DMap'],
     install_requires=[
         'Click','scipy','numpy','matplotlib','pandas','seaborn','statsmodels','cooler','numba','psutil','pyranges','tqdm','deeptools','bwa','pairtools','pybedtools'
